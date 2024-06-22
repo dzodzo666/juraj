8387598958b64860f18ad2f678793143ab79f5eb
dzodzo666 <31371604+dzodzo666@users.noreply.github.com>
2024-06-22 15:52:07 +0200

    powershell convert x265 script

---
+++ convert.ps1
@@ -0,0 +1,3 @@
+﻿Get-ChildItem "C:\Users\psychonaut\Pictures\Exported Videos\" -Name | ForEach-Object {
+  C:\Utils\ffmpeg\bin\ffmpeg -i "C:\Users\psychonaut\Pictures\Exported Videos\$_" -vcodec libx265 -acodec aac "C:\Users\psychonaut\Pictures\Exported Videos\c265_$_"
+}
---
+++ convertarg.ps1
@@ -0,0 +1,5 @@
+﻿$paths=$args[0]
+write-host $paths
+Get-ChildItem "$paths" -Name
+
+Get-ChildItem "$paths" -Name | ForEach-Object -Begin { Start-Sleep -s 10 } -Process { C:\Utils\ffmpeg\bin\ffmpeg -i "$paths\$_" -c:v libx265 "$paths\c265_$_.mkv" } -End { Start-Sleep -s 10 }
